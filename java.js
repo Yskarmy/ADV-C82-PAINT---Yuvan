6eab532c4f5150ee9ce637152ac2397eaf7be0de
Yskarmy <97374383+Yskarmy@users.noreply.github.com>
2022-03-18 18:42:36 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,28 @@
+var mouseEvent = "";
+ canvas - 
+ document.getElementById("myCanvas");
+ ctx= canvas.getContext("2d");
+
+addEventListener("mousedown");
+function my_mousedown ("radius" , "color" , "width")
+{
+var mouseEvent = "mousedown";
+}
+
+addEventListener(mouseup);
+function my_mouseup
+{
+ var mouseEvent = "mouseup";
+} 
+
+addEventListener("mouseleave");
+function my_mouseup ()
+{
+var mouseEvent = "mouseleave"
+}
+
+addEventListener("mousemove");
+function my_mousemove ()
+{
+var mouseEvent = "mouseleave";
+}
